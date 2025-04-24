7a224089915805a8d2c776b78392b691d321f174
Miqdad <ahmadmiqdad27@gmail.com>
2025-04-24 15:56:52 +0700

    add return in controller

--- src/Controller/CustomController.php
+++ src/Controller/CustomController.php
@@ -8,6 +8,6 @@ class CustomController extends FrontendController
 {
     public function portalAction()
     {
-        $this->render('layouts/custom/portal.html.twig');
+        return $this->render('layouts/custom/portal.html.twig');
     }
 }
